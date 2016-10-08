edea2962607bd8a606be43cac973505fdc0affd7
Alex Morega <alex@grep.ro>
2016-10-08 23:49:20 +0300

    extract testing helper

--- testsuite/test_config.py
+++ testsuite/test_config.py
@@ -1,8 +1,14 @@
+from tempfile import NamedTemporaryFile
 from csi import Configuration
 
-def test_listen(tmpdir):
-    config_yaml = tmpdir / 'config.yaml'
-    config_yaml.write("listen: localhost:8000\n")
-    config = Configuration(str(config_yaml))
+def parse_config(config_yaml):
+    with NamedTemporaryFile() as tmp:
+        tmp.write(config_yaml.encode('utf-8'))
+        tmp.flush()
+        tmp.seek(0)
+        return Configuration(tmp.name)
+
+def test_listen():
+    config = parse_config("listen: localhost:8000\n")
     assert config.host == 'localhost'
     assert config.port == 8000
